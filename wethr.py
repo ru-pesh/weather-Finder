e24897e08835d1f1202de3d196a82641a547ac13
rupesh <129289866+ru-pesh@users.noreply.github.com>
2023-10-30 21:12:51 +0545

    Update wethr.py

--- wethr.py
+++ wethr.py
@@ -1,4 +1,4 @@
-import requ
+import reque
 # Requests moduele will help fetching data from the url.
 from bs4 import BeautifulSoup
 # The bs4 moduele and beautifulsoup is imported for filtering data from website.
